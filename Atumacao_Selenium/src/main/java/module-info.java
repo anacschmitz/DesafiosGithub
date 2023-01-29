dea303c29d336f37035970062db8a360650b2f84
anacschmitz <anaschmitzbio@gmail.com>
2023-01-29 17:22:26 -0300

    Atualizando

--- Atumacao_Selenium/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.atumacao_selenium {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.atumacao_selenium to javafx.fxml;
-    exports com.example.atumacao_selenium;
-}
